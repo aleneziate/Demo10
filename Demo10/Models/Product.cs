8540b35db1d4e507297254afed8812c2e684d6db
alene <alene@ABDALLAH>
2024-12-26 09:33:45 +0300

    Add project files.

---
+++ Demo10/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo10.Models
+{
+    public class Product
+    {
+        string name;
+        int price;
+        int quantity;
+        public string GetInfo(string name)
+        {
+            return name;
+
+
+
+
+        }
+        public string GetInfo(string name, int price)
+        {
+
+            return name + ", price " + price;
+
+        }
+
+
+        public string GetInfo(string name , int price , int quantity)
+        {
+         
+            return "Name: "+ name + " price, " + price +"Qty "+ quantity;
+
+
+
+        }
+    }
+}
---
+++ Demo10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Quic;
+using Demo10.Models;
+
+Product p=new Product();
+string name;
+int price, quantity;
+Console.WriteLine("Enter product name");
+name = Console.ReadLine();
+Console.WriteLine("Enter price");
+price  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter quantity ");
+quantity = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( p.GetInfo(name));
+Console.WriteLine(p.GetInfo(name, price));
+Console.WriteLine( p.GetInfo(name, price,quantity) + "Total: " +(price*quantity));
+ 
